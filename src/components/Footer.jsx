c77d78ed50eb227437707f5dd7afa5da038dad29
Gabriel_Deon <gabriel1deon@gmail.com>
2024-06-15 18:44:13 -0300

    feat: Complete Footer and Email Verification function & logic

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,91 @@
+import React from "react";
 
 function Footer() {
+  const [email, setEmail] = React.useState("");
+  const [emailError, setEmailError] = React.useState("");
+
+  function validateEmail($email) {
+    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    return reg.test($email);
+  }
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (validateEmail(email)) {
+      setEmailError("");
+    } else {
+      setEmailError("Email inválido!");
+    }
+  };
+
   return (
-    <div>
-  
+    <div className="footer">
+      <header className="footerContent">
+        <div className="footerContentTop">
+          <div className="fctStreet">
+            <h1>footer.</h1>
+            <p>
+              Rua Alexandre Dumas, 1711 - 6º andar - Chácara Santo Antônio, São
+              Paulo - SP, 04717-004
+            </p>
+          </div>
+          <div className="fctItens">
+            <div className="fctItensLinks">
+              <p>Links</p>
+              <ul>
+                <li>
+                  <a href="">Home</a>
+                </li>
+                <li>
+                  <a href="">Shop</a>
+                </li>
+                <li>
+                  <a href="">About</a>
+                </li>
+                <li>
+                  <a href="">Contact</a>
+                </li>
+              </ul>
+            </div>
+            <div className="fctItensHelp">
+              <p>Help</p>
+              <ul>
+                <li>
+                  <a href="">Payment Options</a>
+                </li>
+                <li>
+                  <a href="">Returns</a>
+                </li>
+                <li>
+                  <a href="">Privacy Policies</a>
+                </li>
+              </ul>
+            </div>
+            <div className="fctNewsletter">
+              <p>Newsletter</p>
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  placeholder="Enter Your Email Address"
+                  name="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                ></input>
+                {emailError && <span className="error">{emailError}</span>}
+                <button type="submit">SUBSCRIBE</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="footerContentBot">
+          <p>2024 Compass UOL</p>
+        </div>
+      </header>
     </div>
   );
 }
