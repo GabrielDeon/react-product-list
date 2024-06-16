50ed70c3414ef811e756af2a8645d5c498480ad1
Gabriel_Deon <gabriel1deon@gmail.com>
2024-06-16 01:18:19 -0300

    feat: Complete Filter button design

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -1,14 +1,55 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSliders } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
 
 function ProductGrid() {
-    return (
-      <div>
-        <div className="gridControler">          
+  const [showOptions, setShowOptions] = React.useState(false);
+  const [selectedFilter, setSelectedFilter] = React.useState("");
+
+  const handleButtonClick = () => {
+    setShowOptions(!showOptions);
+  };
+
+  const handleOptionClick = (filter) => {
+    setSelectedFilter(filter);
+    setShowOptions(false);
+  };
+
+  return (
+    <div>
+      <div className="gridControler">
+        <div className="gridControlerContent">
+          <div className="gccLeft">
+            <div className="gccLeftFilter">
+              <button className="filterButton" onClick={handleButtonClick}>
+                <FontAwesomeIcon className="filterIcon" icon={faSliders} />
+                <p className="filterSpecs">Filter</p>
+              </button>
+              {showOptions && (
+                <div className="filter-options">
+                  <button onClick={() => handleOptionClick("none")}>
+                    {"None"}
+                  </button>
+                  <button onClick={() => handleOptionClick("name")}>
+                    {"Name (A...Z)"}
+                  </button>
+                  <button onClick={() => handleOptionClick("price")}>
+                    {"Price (Higher...Low)"}
+                  </button>
+                </div>
+              )}
+            </div>
+
+            <div className="filterInfo">
+              <p>Showing 1-16 of 32 results</p>
+            </div>
+          </div>
         </div>
-        <div className="gridContent"></div>
-        <div className="gridPageControler"></div>    
       </div>
-    );
-  }
-  
-  export default ProductGrid;
-  
+      <div className="gridContent"></div>
+      <div className="gridPageControler"></div>
+    </div>
+  );
+}
+
+export default ProductGrid;
